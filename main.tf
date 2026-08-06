9e40031c3540612a7835757564079e92cc925923
lojt-cloud <264168469+lojt-cloud@users.noreply.github.com>
2026-08-06 11:29:16 +0200

    Add DynamoDB PITR and S3 public access block, wire up Checkov scan

--- main.tf
+++ main.tf
@@ -42,6 +42,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "data_store" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "state_lock" {
   name         = "${var.project_name}-${var.environment}-lock-table"
   billing_mode = "PAY_PER_REQUEST"
@@ -50,6 +59,10 @@ resource "aws_dynamodb_table" "state_lock" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
   }
 
   tags = {
